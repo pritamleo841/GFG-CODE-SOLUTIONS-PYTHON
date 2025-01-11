7fb8ee11abadd5cf8916f915692a297b42523421
Pritam Sharma <sharmapritam841@gmail.com>
2025-01-11 21:04:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum of Middle elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.py
+++ Difficulty: Medium/Sum of Middle elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.py
@@ -3,7 +3,43 @@
 class Solution:
     def sum_of_middle_elements(self, arr1, arr2):
         # code here
-        #Brute force solution
+        #Using Binary Search Algo - O(logn)
+        #Prerequisite - Median of two sorted arrays
+        n=len(arr1)
+        low,high=0,n
+        
+        while low<=high:
+            mid1=(low+high)//2
+            mid2=n-mid1 #For second array
+            
+            l1=arr1[mid1-1] if mid1>0 else float('-inf')
+            l2=arr2[mid2-1] if mid2>0 else float('-inf')
+            
+            r1=arr1[mid1] if mid1<n else float('inf')
+            r2=arr2[mid2] if mid2<n else float('inf')
+            
+            if l1<=r2 and l2<=r1 :
+                return max(l1,l2)+min(r1,r2)
+            elif l1>r2:
+                high=mid1-1
+            else:
+                low=mid1+1
+        return 0
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Brute force solution; TC:O(2n), SC:O(2n)
         arr3=[]
         i,j=0,0
         n=len(arr1)
