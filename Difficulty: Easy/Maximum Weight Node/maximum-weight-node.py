e42f06bddefa041474dd531a81127ecd0a8b4eb5
Pritam Sharma <sharmapritam841@gmail.com>
2025-01-18 13:32:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Weight Node/maximum-weight-node.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution():
+    def maxWeightCell(self, Edge):
+        #your code goes here
+        n = len(exits)
+        # Initialize weight array with zeros
+        weight = [0]*n
+    
+        # Step 1: Calculate the weights
+        for i in range(n):
+            if exits[i] != -1:
+                weight[exits[i]] += i
+    
+        # Step 2: Find the cell with maximum weight
+        max_wt = float('-inf')
+        max_index = 0
+        for i in range(n):
+            if weight[i] >= max_wt :
+                max_wt = weight[i]
+                max_index = i
+    
+        return max_index
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        exits = [int(i) for i in input().split()]
+        sln = Solution()
+        print(sln.maxWeightCell(exits))
+        print("~")
+
+# } Driver Code Ends
