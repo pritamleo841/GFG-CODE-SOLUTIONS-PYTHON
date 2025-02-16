b4e0549ac102bd518cd44168783e69d62beef143
Pritam Sharma <sharmapritam841@gmail.com>
2025-02-16 20:46:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -2,11 +2,30 @@ class Solution:
     def maxSubArraySum(self, arr):
         # Your code here
         #Kadene's algorithm
+        curr_sum=max_sum=arr[0]
+        start=end=s=0  # Pointers to track the subarray
+    
+        for i in range(1,len(arr)):
+            if arr[i]>curr_sum+arr[i]:  # Start new subarray
+                curr_sum=arr[i]
+                s=i  # Update starting index
+            else:
+                curr_sum+=arr[i]  # Extend current subarray
+            
+            if curr_sum>max_sum:  # Update max sum and subarray indices
+                max_sum=curr_sum
+                start=s
+                end=i
+        #arr[start:end+1] =>maximum subarray sum subarray
+        return max_sum  # Return max sum
+        '''
+        #Kadene's algorithm
         curr_sum = max_sum = arr[0]
         for i in range(1,len(arr)):
             curr_sum = max(arr[i],curr_sum+arr[i])
             max_sum = max(curr_sum,max_sum)
         return max_sum
+        '''
 
 
 #{ 
