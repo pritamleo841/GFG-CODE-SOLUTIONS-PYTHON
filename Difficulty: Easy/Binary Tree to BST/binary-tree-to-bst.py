3a94b266bbe2f3dab5319708a779460768b11fc2
Pritam Sharma <sharmapritam841@gmail.com>
2025-02-27 20:10:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Tree to BST/binary-tree-to-bst.py
@@ -0,0 +1,127 @@
+#User function Template for python3
+
+'''
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+class Solution:
+    # The given root is the root of the Binary Tree
+    # Return the root of the generated BST
+    def binaryTreeToBST(self, root):
+        if not root:
+            return None
+        # Extract all elements of the binary tree and store them in an array.
+        inorder = []
+        def inorderExtract(node):
+            if node:
+                inorderExtract(node.left)
+                inorder.append(node.data)
+                inorderExtract(node.right)
+        inorderExtract(root)
+        # Sort the array in ascending order (BST property).
+        inorder.sort()
+        # Reconstruct the BST by performing an inorder traversal 
+        # and replacing the values.
+        index = 0
+        def inorderReplace(node):
+            nonlocal index
+            if node:
+                inorderReplace(node.left)
+                node.data = inorder[index]
+                index+=1
+                inorderReplace(node.right)
+        inorderReplace(root)
+        return root
+        
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+def printInorder(root): 
+    if root is None: 
+        return
+    printInorder(root.left) 
+    print (root.data, end=' ')  
+    printInorder(root.right) 
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        Solution().binaryTreeToBST(root)
+        printInorder(root)
+        print()
+        print("~")
+# } Driver Code Ends
