5e4d0ef025c3d2115095b36f700ae6787bf3a024
Pritam Sharma <sharmapritam841@gmail.com>
2025-03-16 18:28:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.py
+++ Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.py
@@ -1,110 +1,92 @@
+#User function Template for python3
+
+
+#Function to check if any pair exists in BST whose sum is equal to given value.
+def findPair(root,X):
+    map_x = set()
+    def dfs(node):
+        if not node:
+           return False
+        if (X-node.key) in map_x:
+           return True
+        map_x.add(node.key)
+        return dfs(node.left) or dfs(node.right)
+    return dfs(root)
+
+
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
-from collections import deque
-# Tree Node
+import atexit
+import io
+import sys
+#Contributed by : Nagendra Jha
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+# Node Class:
 class Node:
-    def __init__(self, val):
-        self.right = None
-        self.data = val
+    def __init__(self,val):
+        self.key = val
         self.left = None
-
-# Function to Build Tree   
-def buildTree(s):
-    #Corner Case
-    if(len(s)==0 or s[0]=="N"):           
-        return None
-        
-    # Creating list of strings from input 
-    # string after spliting by space
-    ip=list(map(str,s.split()))
-    
-    # Create the root of the tree
-    root=Node(int(ip[0]))                     
-    size=0
-    q=deque()
-    
-    # Push the root to the queue
-    q.append(root)                            
-    size=size+1 
-    
-    # Starting from the second element
-    i=1                                       
-    while(size>0 and i<len(ip)):
-        # Get and remove the front of the queue
-        currNode=q[0]
-        q.popleft()
-        size=size-1
-        
-        # Get the current node's value from the string
-        currVal=ip[i]
-        
-        # If the left child is not null
-        if(currVal!="N"):
-            
-            # Create the left child for the current node
-            currNode.left=Node(int(currVal))
-            
-            # Push it to the queue
-            q.append(currNode.left)
-            size=size+1
-        # For the right child
-        i=i+1
-        if(i>=len(ip)):
-            break
-        currVal=ip[i]
-        
-        # If the right child is not null
-        if(currVal!="N"):
-            
-            # Create the right child for the current node
-            currNode.right=Node(int(currVal))
-            
-            # Push it to the queue
-            q.append(currNode.right)
-            size=size+1
-        i=i+1
-    return root
-    
-
-# } Driver Code Ends
-
-
-'''
-# Tree Node
-class Node:
-    def __init__(self, val):
         self.right = None
-        self.data = val
-        self.left = None
 
-'''
-from collections import defaultdict
-class Solution:
-    def findTarget(self, root, target): 
-        map_x = set()
-        def dfs(node):
-            if not node:
-               return False
-            if (target-node.data) in map_x:
-               return True
-            map_x.add(node.data)
-            return dfs(node.left) or dfs(node.right)
-        return dfs(root)
+hash = set() # hash to be used.
+# Tree Class
+class BSTree:
+    def __init__(self):
+        self.root = None
 
+    def Insert(self,x):
+        if self.root is None:
+            self.root = Node(x)
+            return
+        curr_node = self.root
+        while curr_node:
+            if curr_node.key < x: # go to right subtree if value is more
+                if curr_node.right is None:
+                    break
+                curr_node = curr_node.right
+            elif curr_node.key > x:   #  go to left subtree if value is more.
+                if curr_node.left is None:
+                    break
+                curr_node = curr_node.left
+            else:
+                return # no duplicate is to be inserted
 
-#{ 
- # Driver Code Starts.
-if __name__=="__main__":
-    t=int(input())
-    for _ in range(0,t):
-        s=input()
-        summ = int(input())
-        root=buildTree(s)
-        ans = Solution().findTarget(root,summ)
-        if (ans==False):
-            print(0)
+        # insert key at the leaf position.
+        if curr_node.key < x:
+            curr_node.right = Node(x)
         else:
+            curr_node.left = Node(x)
+        return
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())  # number of nodes in tree
+        a = list(map(int, input().strip().split()))  # parent child info in list
+        x = int(input())
+        # construct the tree according to given list
+        tree = BSTree()
+        for value in a:
+            tree.Insert(value)  # Insert the nodes in tree.
+        if findPair(tree.root,x):
             print(1)
+        else:
+            print(0)
+        hash = set()
+
+
         print("~")
 # } Driver Code Ends
