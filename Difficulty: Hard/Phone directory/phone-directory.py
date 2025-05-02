ce32e9d8126f423dfe823da47393405de22a91c7
Pritam Sharma <sharmapritam841@gmail.com>
2025-05-02 20:26:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Phone directory/phone-directory.py
+++ Difficulty: Hard/Phone directory/phone-directory.py
@@ -1,7 +1,41 @@
 #User function Template for python3
-
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.words=set()
 class Solution:
+    #TC - O(n*m+L*logk)
+    #where L is length of s and k is number of matched contacts
+    def insert(self,root,word):
+        node=root
+        for char in word:
+            if char not in node.children:
+                node.children[char]=TrieNode()
+            node=node.children[char]
+            node.words.add(word)
+            
+    def prefixSearch(self,root,prefix):
+        node=root
+        for char in prefix:
+            if char not in node.children:
+                return ["0"]
+            node=node.children[char]
+        return sorted(node.words)
+        
     def displayContacts(self, n, contact, s):
+        contact=sorted(set(contact))
+        root=TrieNode()
+        for word in contact:
+            self.insert(root,word)
+        ans=[]
+        for i in range(1,len(s)+1):
+            prefix=s[:i]
+            matches=self.prefixSearch(root,prefix)
+            ans.append(matches)
+        return ans
+        
+        '''
+        #TC - O(nlogn)+O(L*(n*m+nlogn))
         ans = []
         contact = sorted(set(contact))  # remove duplicates and sort once
         for i in range(1,len(s)+1):
@@ -16,6 +50,7 @@ class Solution:
                 temp = sorted(unique)
             ans.append(temp)
         return ans
+        '''
                     
 
 
