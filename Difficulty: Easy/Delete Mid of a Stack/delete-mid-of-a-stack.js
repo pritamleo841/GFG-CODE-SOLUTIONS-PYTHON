daec72fb4a267c72bd8fcfc278d4b41065693eb7
Pritam Sharma <sharmapritam841@gmail.com>
2025-06-18 19:54:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.js
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.js
@@ -1,7 +1,6 @@
 // User function Template for JavaScript
 
 class Solution {
-
     deleteMid(stack) {
         const size=stack.length;
         function recur(count) {
@@ -9,12 +8,10 @@ class Solution {
                 stack.pop(); // Delete middle element
                 return;
             }
-    
             const temp=stack.pop(); // Pop top
             recur(count+1);         // Go deeper
             stack.push(temp);         // Push back after middle is removed
         }
-    
         recur(0);
     }
 }
