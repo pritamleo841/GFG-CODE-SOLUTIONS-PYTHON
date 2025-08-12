8114290f7f423f1f27bfd9493ed19fb3674156a7
Pritam Sharma <sharmapritam841@gmail.com>
2025-08-12 19:32:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.py
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.py
@@ -1,8 +1,23 @@
 # Function should return an integer value
 class Solution:
     def convertFive(self, n):
+        if n==0:
+            return 5
+            
+        place=1
+        res=0
+        while n:
+            rem=n%10
+            if rem==0:
+                rem=5
+            res+=place*rem
+            place*=10
+            n//=10
+        return res
+        '''
         lst=list(str(n))
         for i in range(len(lst)):
             if lst[i]=='0':
                 lst[i]='5'
         return int(''.join(lst))
+        '''
