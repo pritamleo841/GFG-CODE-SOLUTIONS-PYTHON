1e5555f12547d2f1a70e06d57ee68e9cc98b883c
Pritam Sharma <sharmapritam841@gmail.com>
2025-08-13 19:37:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted matrix/sorted-matrix.py
+++ Difficulty: Basic/Sorted matrix/sorted-matrix.py
@@ -1,7 +1,21 @@
 #User function Template for python3
 import heapq
 class Solution:
-    def sortedMatrix(self,n,mat):
+    def sortedMatrix(self,N,Mat):
+        #=O(N^2 \log N) time.
+        # Flatten the matrix into a list
+        flat_list = [Mat[i][j] for i in range(N) for j in range(N)]
+        
+        # Convert the list into a min-heap (O(N^2))
+        heapq.heapify(flat_list)
+        
+        # Extract min elements and put them back into the matrix (O(N^2 log N^2))
+        for i in range(N):
+            for j in range(N):
+                Mat[i][j] = heapq.heappop(flat_list)
+        
+        return Mat
+        '''
         temp=[]
         for i in range(n):
            for j in range(n):
@@ -13,3 +27,4 @@ class Solution:
                 mat[i][j]=temp[k]
                 k+=1
         return mat
+        '''
